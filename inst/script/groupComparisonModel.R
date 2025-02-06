31053a38ac348c2e3272b95b8232f3bb40592200
Tony Wu <wu.anthon@northeastern.edu>
2025-02-06 18:23:35 -0500

    docs(inst): Update sample code that generates test files

--- inst/script/groupComparisonModel.R
+++ inst/script/groupComparisonModel.R
@@ -1,12 +1,10 @@
 # Script to create groupComparisonModel.csv in extdata folder
 library(MSstatsConvert)
-input = system.file("tinytest/raw_data/Metamorpheus/AllQuantifiedPeaks.tsv", 
-                    package = "MSstatsConvert")
-input = data.table::fread(input)
-annot = system.file("tinytest/raw_data/Metamorpheus/Annotation.tsv", 
-                    package = "MSstatsConvert")
-annot = data.table::fread(annot)
-msstats_imported = MetamorpheusToMSstatsFormat(input, annotation = annot)
+input = data.table::fread(system.file(
+    "extdata/msstats.csv",
+    package = "MSstatsBioNet"
+))
+msstats_imported = FragPipetoMSstatsFormat(input, use_log_file = FALSE)
 head(msstats_imported)
 
 library(MSstats)
@@ -15,17 +13,6 @@ groupComparisonResult = groupComparison(contrast.matrix="pairwise",
                                         data=QuantData, 
                                         use_log_file = FALSE)
 
-uniprot_to_hgnc_mapping = c(
-    "B9A064" = "38476",
-    "O00391" = "9756",
-    "O14818" = "9536",
-    "O43598" = "21218",
-    "O43707" = "166",
-    "P16050" = "11390",
-    "P84243" = "4765"
-)
-groupComparisonResult$ComparisonResult$HgncId = uniprot_to_hgnc_mapping[
-    groupComparisonResult$ComparisonResult$Protein
-]
-
-write.csv(groupComparisonResult$ComparisonResult, "groupComparison.csv", row.names = FALSE)
+library(MSstatsBioNet)
+annotated_df = annotateProteinInfoFromIndra(groupComparisonResult$ComparisonResult, "Uniprot")
+write.csv(annotated_df, "groupComparisonModel.csv", row.names = FALSE)
